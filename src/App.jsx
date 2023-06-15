b08416a1a78fbfad0b7fbb8898a136b90784bd64
ashraful2020 <mr.ashraful2020@gmail.com>
2023-06-15 12:58:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import "./App.css";
+import {
+  Bootstrap,
+  Css,
+  Express,
+  Git,
+  Github,
+  Html,
+  Javascript,
+  Linkedin,
+  Mongodb,
+  Mongodb2,
+  Next,
+  Next2,
+  Node,
+  Phone,
+  Portfolio,
+  React,
+  Redux,
+  Scss,
+  Scss2,
+  Tailwind,
+  Trello,
+  Typescript,
+  Vscode,
+} from "./assets";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="main">
+          <Bootstrap />
+          <Css />
+          <Express />
+          <Git />
+          <Github />
+          <Html />
+          <Javascript />
+          <Linkedin />
+          <Mongodb />
+          <Mongodb2 />
+          <Next />
+          <Next2 />
+          <Node />
+          <Phone />
+          <Portfolio />
+          <React />
+          <Redux />
+          <Scss />
+          <Scss2 />
+          <Tailwind />
+          <Trello />
+          <Typescript />
+          <Vscode />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/icons/trello.jsx
@@ -0,0 +1,33 @@
+
+export default function Trello() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="800"
+      height="800"
+      fill="none"
+      viewBox="0 0 32 32"
+    >
+      <path
+        fill="url(#a)"
+        fillRule="evenodd"
+        d="M25.013 4H7.002A3.001 3.001 0 0 0 4 7v18a3 3 0 0 0 3.002 3h18.011A3.001 3.001 0 0 0 28 25V7a3 3 0 0 0-2.987-3ZM9 7h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H9a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2Zm14 0h-4a2 2 0 0 0-2 2v7a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2Z"
+        clipRule="evenodd"
+      />
+      <defs>
+        <linearGradient
+          id="a"
+          x1="16.008"
+          x2="16.008"
+          y1="28"
+          y2="4"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#0052CC" />
+          <stop offset=".517" stop-color="#217EF8" />
+          <stop offset="1" stop-color="#2684FF" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
